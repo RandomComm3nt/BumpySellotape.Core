6a99e8576d0901438da5e2396846d07942138167
Dan Armour <randomcomm3nt@hotmail.co.uk>
2022-01-21 16:28:36 +0000

    Added input handler

---
+++ Scripts/Input/InputHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BumpySellotape.Core.Input
+{
+    public abstract class InputHandler<T> : MonoBehaviour
+    {
+        public abstract void Initialise(T controller);
+    }
+}
---
+++ Scripts/Input/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BumpySellotape.Core.Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        public InputHandler<TController> SetInputHandler<THandler, TController>(TController controller)
+            where THandler : InputHandler<TController>
+        {
+            var go = new GameObject(typeof(THandler).Name);
+            var handler = go.AddComponent<THandler>();
+            handler.Initialise(controller);
+            go.transform.SetParent(transform);
+            return handler;
+        }
+    }
+}
