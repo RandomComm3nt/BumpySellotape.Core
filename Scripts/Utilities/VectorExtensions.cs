09f9ef1b2dcf1e9be5d56fee06d259bb0325e7ea
Dan Armour <randomcomm3nt@hotmail.co.uk>
2022-02-24 18:38:16 +0000

    VectorExtensions

---
+++ Scripts/Utilities/VectorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BumpySellotape.Core.Utilities
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 WithZ(this Vector3 v, float z)
+        {
+            return new Vector3(v.x, v.y, z);
+        }
+    }
+}
